85de416ccc764ee6e8cf10bf484af2d38f65d184
Ishraq Haider Chowdhury <ishraq.h.c@gmail.com>
2021-01-03 17:55:41 +0100

    A simple pic sharing app

---
+++ posts/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Post(models.Model):
+    image = models.ImageField()
+    description = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
