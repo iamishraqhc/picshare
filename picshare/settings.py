39a4cfee8bf79b245fe770df52d3a36b725ba2c2
Ishraq Haider Chowdhury <ishraq.h.c@gmail.com>
2021-01-03 18:11:10 +0100

    Secured!

--- picshare/settings.py
+++ picshare/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+from key import SECRET_KEY
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sh)z32u*)s=(0(oop0&ik6vqi2s2j##49t89753kp4%%iqkd+w'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
